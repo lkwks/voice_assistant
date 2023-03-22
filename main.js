3dba729bc559aa7e05707a6ab1213a25d3d31886
Kangwook Lee <lkwks@naver.com>
2023-03-23 02:48:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ class Messages{
         this.messages.push({role: "user", content: content});
         this.messages_token.push(content.split(" ").length * 5);
         this.flush_if_too_many_tokens();
-        chatgpt_api([this.system_message, ...this.messages]);
+        var answer = await chatgpt_api([this.system_message, ...this.messages]);
+        this.messages.push({role: "assistant", content: answer});
     }
-    
 
     flush_if_too_many_tokens()
     {
@@ -50,48 +50,76 @@ class Messages{
 
 }
 
+class AnswerStream{
+    constructor()
+    {
+        this.now_streaming = false;
+        this.now_answer = "";
+    }
+    
+    start()
+    {
+        if (this.now_streaming === false)
+        {
+            this.now_answer = "";
+            this.now_streaming = true;
+        }
+    }
+    
+    add_answer(answer)
+    {
+        this.now_answer += answer;
+    }
+    
+    end()
+    {
+        this.now_streaming = false;
+    }
+}
+
 var messages = new Messages();
+var answer_stream = new AnswerStream();
+
+// 중간중간에 answer_stream.now_answer의 문장이 끝났는지를 정규식으로 확인한 후 끝났다면 구글 TTS API에 전송하는 setInterval 코드를 추가할 수 있을 듯함.
 
 async function chatgpt_api(messages)
 {   
-    fetch("https://api.openai.com/v1/chat/completions", {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${localStorage.getItem("API_KEY")}`,
         },
         body: JSON.stringify({ model: "gpt-3.5-turbo", messages: messages, stream: true})
-    }).then(response => {
+    });
   
         const reader = response.body.getReader();
         let buffer = '';
 
-        reader.read().then(function processResult(result) {
+        return await reader.read().then(function processResult(result) {
 
           buffer += new TextDecoder('utf-8').decode(result.value || new Uint8Array());
             
           var messages = buffer.split('\n\n')
           buffer = messages.pop();
-          console.log(buffer, messages.length);
-          if (messages.length === 0) return;
+          if (messages.length === 0) 
+          {
+              answer_stream.end();
+              return answer_stream.now_answer;
+          }
 
           messages.forEach(message => {
              if (message.includes("data: ") && message.includes("[DONE]") === false)
              {
+                 answer_stream.start();
                  const val = JSON.parse(message.replace("data: ", ""));
                  if (val.choices[0].delta.content)
-                     document.querySelector("div.answer").innerHTML += val.choices[0].delta.content;
-                 else
-                     console.log(val);
+                     answer_stream.add_answer(val.choices[0].delta.content);
              }
           });
           
-          reader.read().then(processResult);
+          return await reader.read().then(processResult);
         });
-   }).catch(error => {
-       console.error(error);
-   });
-
 }
 
 async function whisper_api(file)
@@ -111,16 +139,15 @@ async function whisper_api(file)
     return await response.json();
 }
 
-function start_recording()
+async function start_recording()
 {
-      navigator.mediaDevices.getUserMedia({ audio: true })
-      .then( stream => {
-        mediaRecorder = new MediaRecorder(stream, {type: 'audio/webm'});
-        mediaRecorder.start();    
+    const stream = navigator.mediaDevices.getUserMedia({ audio: true });
+      
+    mediaRecorder = new MediaRecorder(stream, {type: 'audio/webm'});
   
-        mediaRecorder.ondataavailable = e => chunks.push(e.data);
+    mediaRecorder.ondataavailable = e => chunks.push(e.data);
   
-        mediaRecorder.onstop = async e => {
+    mediaRecorder.onstop = async e => {
           var blob = new Blob(chunks, { 'type' : 'audio/webm' });
           var file = new File([blob], "audio.webm", { type: "audio/webm;" });
           var result = await whisper_api(file);
@@ -128,8 +155,10 @@ function start_recording()
           messages.send_chatgpt(result.text);
           chunks = [];
           mediaRecorder = null;
-        };
-      });
+          stream.getTracks().forEach(track => track.stop());
+    };
+
+    mediaRecorder.start();    
 }
 
 var mediaRecorder = null, chunks = [];
