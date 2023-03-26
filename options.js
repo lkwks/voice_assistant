f07fbd65c1119548d7b073f4d63354c8ef9b9217
Kangwook Lee <lkwks@naver.com>
2023-03-26 20:50:17 +0900

    Update options.js

--- options.js
+++ options.js
@@ -1,15 +1,15 @@
 
 if (localStorage.getItem("SYSTEM_MESSAGE"))
-    document.querySelector("div.SYSTEM_MESSAGE > textarea").innerText = localStorage.getItem("SYSTEM_MESSAGE");
+    document.querySelector("div.SYSTEM_MESSAGE  textarea").innerText = localStorage.getItem("SYSTEM_MESSAGE");
 
 if (localStorage.getItem("TTS_API_KEY"))
-    document.querySelector("div.TTS_API_KEY > input").value = localStorage.getItem("TTS_API_KEY");
+    document.querySelector("div.TTS_API_KEY  input").value = localStorage.getItem("TTS_API_KEY");
 
 if (localStorage.getItem("API_KEY"))
-    document.querySelector("div.API_KEY > input").value = localStorage.getItem("API_KEY");
+    document.querySelector("div.API_KEY  input").value = localStorage.getItem("API_KEY");
 
 if (localStorage.getItem("SRC"))
-    document.querySelector("div.SRC > input").value = localStorage.getItem("SRC");
+    document.querySelector("div.SRC  input").value = localStorage.getItem("SRC");
 
 document.body.addEventListener("click", e => {
     if (e.target.nodeName === "BUTTON")
