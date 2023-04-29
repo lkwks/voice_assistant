edd66578bb5a8695351fbe8254d4d102e9eddacf
lkwks <lkwks@naver.com>
2023-04-30 05:45:04 +0900

    Enable Eleven Labs

--- options.js
+++ options.js
@@ -5,6 +5,16 @@ if (localStorage.getItem("SYSTEM_MESSAGE"))
 if (localStorage.getItem("TTS_API_KEY"))
     document.querySelector("div.TTS_API_KEY  input").value = localStorage.getItem("TTS_API_KEY");
 
+
+if (localStorage.getItem("API_SERVICE_PROVIDER"))
+    document.querySelector("div.API_SERVICE_PROVIDER  input").value = localStorage.getItem("API_SERVICE_PROVIDER");
+
+if (localStorage.getItem("TTS_API_KEY_ELEVENLABS"))
+    document.querySelector("div.TTS_API_KEY_ELEVENLABS  input").value = localStorage.getItem("TTS_API_KEY_ELEVENLABS");
+
+if (localStorage.getItem("VOICE_ID_ELEVENLABS"))
+    document.querySelector("div.VOICE_ID_ELEVENLABS  input").value = localStorage.getItem("VOICE_ID_ELEVENLABS");
+
 if (localStorage.getItem("API_KEY"))
     document.querySelector("div.API_KEY  input").value = localStorage.getItem("API_KEY");
 
@@ -23,5 +33,11 @@ document.body.addEventListener("click", e => {
             localStorage.setItem("SRC", parentNode.querySelector("input").value);
         if (parentNode.classList.contains("SYSTEM_MESSAGE"))
             localStorage.setItem("SYSTEM_MESSAGE", parentNode.querySelector("textarea").value);
+        if (parentNode.classList.contains("API_SERVICE_PROVIDER"))
+            localStorage.setItem("API_SERVICE_PROVIDER", parentNode.querySelector("input").value);
+        if (parentNode.classList.contains("TTS_API_KEY_ELEVENLABS"))
+            localStorage.setItem("TTS_API_KEY_ELEVENLABS", parentNode.querySelector("input").value);
+        if (parentNode.classList.contains("VOICE_ID_ELEVENLABS"))
+            localStorage.setItem("VOICE_ID_ELEVENLABS", parentNode.querySelector("input").value);
     }
 });
